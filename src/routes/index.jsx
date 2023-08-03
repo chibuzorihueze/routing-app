0f5cf8c2058dabf65e90ecb4f626c37e9176855e
chibuzor ihueze <krisykord@gmail.com>
2023-08-03 11:28:05 +0100

    creating more folder structure

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import UserDashboard from "../pages/app/user/dashboard";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>
-    }
-])
-
-
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "/user",
+        children: [
+          {
+            index: true,
+            element: <UserDashboard />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
